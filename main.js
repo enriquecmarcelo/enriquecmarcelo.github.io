52f12a07d3104662129e31740003722a8f3ab644
Enrique Marcelo <enriquecmarcelo@gmail.com>
2020-08-26 14:20:32 +0800

    cleaned up scripts

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// let pageTop = document.getElementById("top");
-
-// window.onbeforeunload = function () {
-//     window.scrollTo(0, 0);
-// }
-
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function() {
